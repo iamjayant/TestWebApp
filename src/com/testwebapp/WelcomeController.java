9e774ce7059cc7386405abb1577dabb31e7a415c
ROHIT G <bablu.rohit@hotmail.com>
2016-07-08 15:16:07 +0530

    test

--- src/com/testwebapp/WelcomeController.java
+++ src/com/testwebapp/WelcomeController.java
@@ -1,8 +1,19 @@
 package com.testwebapp;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
 
 public class WelcomeController extends AbstractController{
+@Override	
+protected ModelAndView handleRequestInternal(HttpServletRequest request,
+		HttpServletResponse response) throws Exception {
 	
+	ModelAndView modelandview = new ModelAndView("welcome");
+	modelandview.addObject("welcomeMessage", "Welcome to the test application");
+	return modelandview;
+}
 
 }
